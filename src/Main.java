b2f6e5ad56327196fe878bc4c0419302ab1a38b1
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-03-07 20:49:42 -0300

    Criação do código

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+	static int tamanho;
+
+	public static void checar(int num, int qtdVetor) {
+		tamanho = qtdVetor;
+		if (num % 2 == 0) {
+			ThreadVetor.start();
+		} else {
+			ThreadVetor2.start();
+		}
+	}
+	static Thread ThreadVetor = new Thread() {
+		public void run() {
+			int[] vet = new int[tamanho];
+			double tempoInicial = System.nanoTime();
+			for (int i=0;i<vet.length;i++) {
+					vet[i] = 0;
+			}
+			double tempoFinal = System.nanoTime();
+			double tempoTotal = tempoFinal - tempoInicial;
+			tempoTotal = tempoTotal / Math.pow(10, 9);
+			System.out.println(tempoTotal + " s.");
+		}
+	};
+	static Thread ThreadVetor2 = new Thread() {
+		public void run() {
+		}
+	};
+	public static void main(String[] args) {
+		int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor"));
+		int valor = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor"));
+		checar(valor, tamanho);
+	}
+}
